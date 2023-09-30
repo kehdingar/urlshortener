d4a5eac6167cbe433cf9bc39df088e7861dd83be
Kehdinga Raymond <kehdingar@gmail.com>
2023-09-30 05:15:21 +0100

    feat: add access to swagger docs via nginx

--- backend/app/main.py
+++ backend/app/main.py
@@ -1,20 +1,42 @@
 from fastapi import FastAPI, Response
 from fastapi.middleware.cors import CORSMiddleware
-
 from app.models import url
-
 from app.database.config import engine
-
 from app.api import shortener
-
 from prometheus_client import generate_latest, CONTENT_TYPE_LATEST
 
 # Code to enable prometheus.py middleware
 from middleware.prometheus import track_requests
 
 
-
 app = FastAPI(root_path="/api/v1")
+
+from fastapi.openapi.utils import get_openapi
+
+
+def custom_openapi():
+    if app.openapi_schema:
+        return app.openapi_schema
+
+    openapi_schema = get_openapi(
+        title="URL Shortener",
+        version="1.0.0",
+        description="Takes long URLs and shortens them",
+        routes=app.routes,
+    )
+
+    # Add the prefix to every path in the paths section
+    prefixed_paths = {}
+    for path, path_item in openapi_schema["paths"].items():
+        prefixed_path = f"/api/v1{path}" if not path.startswith("/api/v1") else path
+        prefixed_paths[prefixed_path] = path_item
+
+    openapi_schema["paths"] = prefixed_paths
+
+    app.openapi_schema = openapi_schema
+    return app.openapi_schema
+
+app.openapi = custom_openapi
 
 url.Base.metadata.create_all(bind=engine)
 
@@ -29,6 +51,7 @@ async def metrics():
 # Define the CORS middleware
 origins = [
     "http://localhost:80",
+    "http://localhost",
     "http://localhost:8000",
     "http://localhost:8080",
     "http://localhost:3000",
